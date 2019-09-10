ea6708d94e883b515ce4a7ee76eedf93d2f635c8
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-9.local>
2019-09-10 09:38:21 -0500

    adding the login view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './screens/dashboard/dashboard.component';
 import { HomeComponent } from './screens/dashboard/screens/home/home.component';
+import { LoginComponent } from './screens/dashboard/screens/login/login.component';
 
 
 const routes: Routes = [ {path : '', redirectTo: 'dashboard', pathMatch: 'full'},
   { path: 'dashboard', component: DashboardComponent,
                         children: [
-                          { path: '', component: HomeComponent }
-                          ]}];
+                          { path: '', component: HomeComponent },
+                          { path: 'login', component: LoginComponent }
+                        ]
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { DashboardComponent } from './screens/dashboard/dashboard.component';
 import { NavbarComponent } from './screens/dashboard/components/navbar/navbar.component';
 import { FooterComponent } from './screens/dashboard/components/footer/footer.component';
 import { HomeComponent } from './screens/dashboard/screens/home/home.component';
 import { CardComponent } from './components/card/card.component';
+import { LoginComponent } from './screens/dashboard/screens/login/login.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +19,13 @@ import { CardComponent } from './components/card/card.component';
     NavbarComponent,
     FooterComponent,
     HomeComponent,
-    CardComponent
+    CardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
